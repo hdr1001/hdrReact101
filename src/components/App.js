ed1f75c6e011e09f5658ea9d9c52dd55c443670b
Hans de Rooij <HansDeRooij@Hotmail.com>
2021-09-10 20:48:55 +0200

    Introducing JSX test code https://bit.ly/3E2ruxn

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-const App = () => ( 
-   <div>
-      <h1>Hello React</h1>
-   </div>
-);
+const App = () => {
+   const strMdf = 'my dear friends';
+   const langEs = 'es';
+   const colorRed = {color: 'red'};
+   const reactLogoUrl = 'https://reactjs.org/logo-og.png';
+   const hdrBlogUrl = 'https://hdr1001.github.io/blog/';
+
+   const jsxElem = []; //Array of JSX elements
+
+   //A simple JSX example
+   jsxElem.push(<h1>Hello world!</h1>);
+
+   //Embedding expressions in JSX
+   jsxElem.push(<h1>Hello {strMdf}!</h1>);
+   jsxElem.push(<h1>Hello {'y' + 'o' + 'u'}!</h1>);
+   jsxElem.push(<h1>Hello {(param => param)('reader')}!</h1>);
+
+   //Specifying attributes with JSX
+   jsxElem.push(<p lang="nl">Hallo wereld!</p>);
+   jsxElem.push(<p lang={langEs}>Olá Mundo!</p>);
+   jsxElem.push(<p style={colorRed}>Hello world!</p>);
+
+   //Empty tag
+   jsxElem.push(<img src={reactLogoUrl} alt="React logo" height="25%" width="25%" />);
+
+   //JSX elements may contain children
+   jsxElem.push(<p>My <a href={hdrBlogUrl} title="My blog">blog</a> on GitHub</p>);
+
+   return (
+      <div>
+         {jsxElem[jsxElem.length - 1]}
+      </div>
+   );
+};
 
 export default App;
